1d83cf7c47f9f242221b6613a175206dfa53b928
JayMaier <jamesmai@andrew.cmu.edu>
2023-03-06 18:00:13 -0500

    this seems to build cleanly, trying one more time to confirm.

--- ws/ros_ws/build/orbslam-deblur-pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/developer/slam_ws/ros_ws/src/orbslam-deblur-pkg
-# Build directory: /home/developer/slam_ws/ros_ws/build/orbslam-deblur-pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
